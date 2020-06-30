58da45d389ecc069a21a43f58c069312af6fa86d
Fea-Sin <1210992614@qq.com>
2020-06-30 18:30:02 +0800

    v-1 change

--- examples/area.js
+++ examples/area.js
@@ -15,7 +15,8 @@ reactContainer.style.cssText = `
 const config = {
   forceFit: true,
   padding: [100, 50, 50, 30],
-  color: ['#FFD378', '#647BFC', '#DCE1FF', '#FF899D'],
+  color: ['l(90) 0:#5B74FF 1:#E4E8FF', 'l(90) 0:#FACC14 1:#FFF1D0', 'l(90) 0:#F5506C 1:#FFEEEE'],
+
   area: {
     position: 'feature*value',
     color: 'phone',
@@ -38,6 +39,28 @@ const config = {
     option: {
       position: 'top-right',
       offsetY: -20,
+      marker: 'circle',
+      items: [
+        {
+          value: 'iPhone',
+          marker: {
+            symbol: 'circle',
+            fill: '#7F92FF'
+          }
+        }, {
+          value: 'Samsung',
+          marker: {
+            symbol: 'circle',
+            fill: '#FFD378'
+          }
+        }, {
+          value: 'Nokia Smartphone',
+          marker: {
+            symbol: 'circle',
+            fill: '#FF899D'
+          }
+        }
+      ]
     },
   },
   empty: <div style={{color: 'green', fontSize: 30}}>no data</div>
